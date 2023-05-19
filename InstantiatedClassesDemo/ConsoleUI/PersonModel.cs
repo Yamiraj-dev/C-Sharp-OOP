8ab64d44e58ffbe793ff6be26e06a889b9495022
Yamiraj <yamiraj.dev@gmail.com>
2023-05-20 03:35:13 +0800

    Learnt about instantiated classes and its difference from static classes. This is typically used for holding data of an instance (hence the name.)

---
+++ InstantiatedClassesDemo/ConsoleUI/PersonModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+
+    // We can think of this model class as a blueprint.
+
+    public class PersonModel
+    {
+        /*public string firstName;
+        public string lastName;
+        public string emailAddress;*/
+        
+
+        // Auto properties:
+        // - type prop and hit tab twice
+        // Naming is in Pascal case and not Camel case like previously shown above.
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public bool HasBeenGreeted { get; set; }
+    }
+}
+
+// We should practice in general separating classes with data and those with methods.
+// Very few instances static classes have data, and this is in an instantiated class.
+// It holds up multiple pieces of data together, so they don't get mismatched for however long we need them to be around.
---
+++ InstantiatedClassesDemo/ConsoleUI/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // We are using the blueprint here.
+            // These are two different instances of the same model class PersonModel.
+            // This allows us to save multiple data of the same variables together and they don't get mixed with one another.
+
+            /*PersonModel person = new PersonModel();
+
+            person.firstName = "Mus";
+
+            PersonModel secondPerson = new PersonModel();
+
+            secondPerson.firstName = "Mulan"
+            
+            Console.WriteLine(person.firstName);
+            Console.WriteLine(secondPerson.firstName);
+            
+            */
+
+            // Another efficient way of using this would be through a List.
+
+            /*List<PersonModel> people = new List<PersonModel>();   
+
+            PersonModel person = new PersonModel();
+            person.firstName = "Mus";
+            people.Add(person);
+
+            person = new PersonModel();
+            person.firstName = "Mulan";
+            people.Add(person); 
+
+            foreach (PersonModel p in people)
+            {
+                Console.WriteLine(p.firstName);
+            }*/
+
+
+            List<PersonModel> people = new List<PersonModel>();
+            string firstName = "";
+            
+
+            do
+            {
+                Console.Write("What is your first name (or type exit): ");
+                firstName = Console.ReadLine();
+
+                Console.Write("What is your last name: ");
+                string lastName = Console.ReadLine();
+
+                if (firstName.ToLower() != "exit")
+                {
+                    PersonModel person = new PersonModel();
+                    person.FirstName = firstName;
+                    person.LastName = lastName;
+                    people.Add(person);
+                }
+            } while (firstName.ToLower() != "exit");
+
+            foreach (PersonModel p in people) 
+            {
+                ProcessPerson.GreetPerson(p);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+// Class instances will be used a lot for data and methods.
+// Static classes are for stateless processing.
+// This is in general, there are a few edge cases or exceptions to these rules.
