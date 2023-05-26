9006a9525f1f5740d58987f73c1bed1a0f53dae7
Yamiraj <yamiraj.dev@gmail.com>
2023-05-27 01:17:13 +0800

    Learnt about access modifiers. Public, private, protected, internal, protected internal and private protected.

---
+++ AccessModifiersDemoApp/AccessModifiersDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using DemoLibrary;
+
+namespace AccessModifiersDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            /*DataAccess data = new DataAccess();*/ // inaccessible to internal class
+
+            person.SavePerson();
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class ModifiedDataAccess : DataAccess
+    {
+        public void GetUnsecureConnectionInfo()
+        {
+            GetConnectionString(); // still gains access to this method as you inherit from a protected internal method.
+        }
+    }
+
+    public class CEO : Manager
+    {
+        public void GetConnectionInfo()
+        {
+           ModifiedDataAccess modifiedDataAccess = new ModifiedDataAccess();
+
+            modifiedDataAccess.GetUnsecureConnectionInfo(); // now we can accesss something internal even though this class does not inherit from DataAccess, we call the method that does.
+            
+            formerLastName = "accessible";
+        }
+    }
+}
---
+++ AccessModifiersDemoApp/DemoLibrary/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// internal allows anything in this assembly to talk to DataAccess, but nothing outside.
+// most common access modifiers are public and private
+
+namespace DemoLibrary
+{
+    public class DataAccess 
+    {
+        protected internal string GetConnectionString()
+        {
+            return "Sensitive Data";
+        }
+    }
+
+    public class Person
+    {
+
+        private protected string formerLastName = ""; // the protected allows us access as long as it derives from it (child class/inheritance), or within the same class.
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        private string _ssn;
+
+        public string SSN
+        {
+            get 
+            { 
+                return $"***-**-{_ssn.Substring(_ssn.Length - 4)}"; 
+            }
+            set 
+            {
+                _ssn = value; 
+            }
+        }
+
+        public void ChangeLastName(string newLastName)
+        {
+            formerLastName = newLastName;
+            LastName = newLastName;
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"Hello {FirstName}");
+        }
+
+        public void SavePerson()
+        {
+            DataAccess data = new DataAccess();
+            string conn = data.GetConnectionString();
+            // save person data
+        }
+    }
+
+    public class Employee : Person
+    {
+        public string GetFormerLastName()
+        {
+            return formerLastName;
+        }
+    }
+
+    public class Manager : Person
+    {
+        public string GetFormerLastName()
+        {
+            return formerLastName;
+        }
+    }
+}
