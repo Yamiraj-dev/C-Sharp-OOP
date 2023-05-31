47bc443799e090b9c6e63dfe7de7144ebbe6e0a4
Yamiraj <yamiraj.dev@gmail.com>
2023-05-31 10:39:34 +0800

    Learnt about extension methods, it makes code easier to read and follow.  "this" keyword is very important.

---
+++ ExtensionMethodsApp/ExtensionMethods/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ExtensionMethods
+{
+
+    // extension methods append on code and makes it easier to understand/read
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HotelRoomModel room = new HotelRoomModel();
+
+            room.TurnOnAir().SetTemperature(82).OpenShades();
+
+            room.TurnOffAir().CloseShades();
+
+            "HelloWorld".PrintToConsole();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class HotelRoomModel
+    {
+        public int Temperature { get; set; }
+        public bool IsAirRunning { get; set; }
+        public bool AreShadesOpen { get; set; }
+
+    }
+
+    public static class ExtensionSamples
+    {
+        public static void PrintToConsole(this string message) // "this" keyword is critical
+        {
+            Console.WriteLine(message);
+        }
+
+        public static HotelRoomModel TurnOnAir(this HotelRoomModel room)
+        {
+            room.IsAirRunning = true;
+            return room;
+        }
+
+        public static HotelRoomModel TurnOffAir(this HotelRoomModel room)
+        {
+            room.IsAirRunning = false;
+            return room;
+        }
+
+        public static HotelRoomModel SetTemperature(this HotelRoomModel room, int temperature)
+        {
+            room.Temperature = temperature;
+            return room;
+        }
+
+        public static HotelRoomModel OpenShades(this HotelRoomModel room)
+        {
+            room.AreShadesOpen = true;
+            return room;
+        }
+
+        public static HotelRoomModel CloseShades(this HotelRoomModel room)
+        {
+            room.AreShadesOpen = false;
+            return room;
+        }
+    }
+}
